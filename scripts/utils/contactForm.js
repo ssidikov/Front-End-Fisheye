eaa27e3809965dc125019233c2e8cb22dce7adf6
ssidikov <sardorbek.sidikov7@gmail.com>
2024-05-20 00:52:30 +0200

    adaptive styles

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,11 +1,21 @@
 const modal = document.getElementById('contact_modal')
+const body = document.querySelector('body')
 
-function displayModal () {
+// function displayName () {
+//   const photographerName = localStorage.getItem('photographer')
+//   const nameDiv = document.createAttribute('div')
+//   nameDid = setAttribute('class', 'photographer__name')
+//   nameDiv.textContent = photographerName
+// }
+
+function displayModal() {
+  body.style.overflow = 'hidden'
   modal.style.display = 'flex'
   modal.setAttribute('aria-hidden', 'false')
   modal.focus()
 }
-function closeModal () {
+function closeModal() {
+  body.style.overflow = 'auto'
   modal.style.display = 'none'
   modal.setAttribute('aria-hidden', 'true')
 }
