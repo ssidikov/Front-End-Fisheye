b2fc77379e7fc7d639574b55f052357eb8b19860
ssidikov <sardorbek.sidikov7@gmail.com>
2024-06-03 09:31:31 +0200

    Additional settings for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import js from "@eslint/js";
+import globals from 'globals';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+
+  js.configs.recommended,
+
+  {
+    
+      languageOptions: {
+          globals: globals.browser,
+      },
+      rules: {
+          "no-unused-vars": "warn",
+          "no-undef": "warn"
+      }
+
+  }
 ];
